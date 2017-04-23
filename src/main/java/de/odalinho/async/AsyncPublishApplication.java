b2eeb6557e018469c105736e7adc82a8a4d86fad
Dusan Odalovic <dusan.odalovic@whitewall.com>
2017-04-23 09:40:08 +0200

    Initial commit

---
+++ src/main/java/de/odalinho/async/AsyncPublishApplication.java
@@ -0,0 +1,14 @@
+package de.odalinho.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncPublishApplication.class, args);
+	}
+}
+
+
